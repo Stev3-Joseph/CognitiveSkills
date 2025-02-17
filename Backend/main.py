fb26721fb032bccb6fbc49a39435d02653626266
Steve Joseph <stevejoseph2005@gmail.com>
2025-02-17 22:39:10 +0530

    cors

--- Backend/main.py
+++ Backend/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 from endpoint import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Assessment API")
 app.include_router(router, prefix="/api/v1")
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # In production, replace with your frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # uvicorn main:app --reload
 # myenv\Scripts\activate 
